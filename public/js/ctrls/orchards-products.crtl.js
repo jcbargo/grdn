ecaafa4dbb381d5708a854e11cc4ca3dda31c193
Julio Barboza <jcbargo@gmail.com>
2016-08-27 18:45:56 -0600

    final product details

--- public/js/ctrls/orchards-products.crtl.js
+++ public/js/ctrls/orchards-products.crtl.js
@@ -23,6 +23,32 @@
 
                 $('.js-products-list').append($(this));
             });
+
+            $('.js-product-lookup').on("keydown", function(event) {
+                if (event.which == 13) {
+                    var productName = $('.js-product-lookup').val().toLowerCase(),
+                        markup =
+                            '<div class="card">' +
+                               '<div class="card-block">' +
+                                   '<div class="media">' +
+                                       '<div class="media-left">' +
+                                           '<div class="color-tag orange"></div>' +
+                                       '</div>' +
+                                       '<div class="media-body">' +
+                                           '<p class="orchard-name">' + productName + '</p>' +
+                                           '<div class="card-text">' +
+                                                '<ul>' +
+                                                    '<li>Ayuda a bajar de peso</li>' +
+                                               '</ul>' +
+                                           '</div>' +
+                                       '</div>' +
+                                   '</div>' +
+                               '</div>';
+
+                    $('.js-products-list').append(markup);
+                    $('.js-product-lookup').val('');
+                }
+            });
         });
     }
 })();
